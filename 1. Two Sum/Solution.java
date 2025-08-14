dfe41d018d7d61757d20a696e6c4c39e19a4e18a
Swapnil Dhagdi <156108746+SwapnilDhagdi@users.noreply.github.com>
2025-08-14 23:42:25 +0530

    Update Solution.java

--- 1. Two Sum/Solution.java
+++ 1. Two Sum/Solution.java
@@ -16,3 +16,22 @@ class Solution {
         return new int[]{};
     }
 }
+
+---------------------------------------------------------------------------------------------------------OR------------------------------------------------------------------------------------------------------------
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map=new LinkedHashMap<>();
+        if(nums.length==0)
+            return new int[]{};
+        if(nums.length==2)
+            return new int[]{0,1};
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[]{map.get(target-nums[i]),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
