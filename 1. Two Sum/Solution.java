0b522bd1c351ee63dd7fc23fb04d6673a3e21ae8
Swapnil Dhagdi <156108746+SwapnilDhagdi@users.noreply.github.com>
2025-08-14 23:28:04 +0530

    Create Solution.java

---
+++ 1. Two Sum/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if(nums.length==0)
+        return new int[]{};
+
+        if(nums.length==2)
+        return new int[]{0,1};
+
+        for(int i=0;i<=nums.length-2;i++){
+            for(int j=i+1;j<=nums.length-1;j++){
+                if((nums[i]+nums[j])==target){
+                    return new int[]{i,j};
+                }
+            } 
+        }
+        return new int[]{};
+    }
+}
