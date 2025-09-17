f56e2fbe080ae108b2061ab27019e178382f8a52
Swapnil Dhagdi <156108746+SwapnilDhagdi@users.noreply.github.com>
2025-09-17 17:39:52 +0530

    Create Solution.java

---
+++ 11. Container With Most Water/Solution.java
@@ -0,0 +1,18 @@
+/*Approach 2pointer O(n) */
+class Solution {
+     public static int maxArea(int[] height) {
+        if(height.length==0 || height.length==1||height==null)return 0; //edge case
+         int left=0,right=height.length-1,Area=0; 
+        while(left<right){
+            if(Area<Math.min(height[left],height[right])*(right-left)){ // if area is gerater than the current width area of container the area will change 
+                Area=Math.min(height[left],height[right])*(right-left);
+            }
+            if(height[left]<height[right]){ //changing the pointer location as per the largest height of pillar ,one with low height changed 
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return Area;
+    }
+}
